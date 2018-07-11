691139115173487bd8f63e14b03c5452cd276453
Erica Salling <esalling23@gmail.com>
2018-07-11 14:16:37 -0400

    fix for code recording

--- skills/code_entered.js
+++ skills/code_entered.js
@@ -6,21 +6,21 @@ var sort = require("sorted-object");
 const correctButtonCodes = {
   random: ['red', 'red', 'green', 'grey', 'grey', 'green', 'green', 'red', 'grey'],
   safari: ['grey','green','green', 'grey', 'grey', 'red', 'red', 'green', 'red'],
-  hole: ['grey', 'grey', 'red', 'red', 'red', 'green', 'grey', 'red', 'grey'], 
+  hole: ['grey', 'grey', 'red', 'red', 'red', 'green', 'grey', 'red', 'grey'],
   glyph: ['grey', 'green', 'grey', 'grey', 'grey', 'grey', 'grey', 'grey', 'red'],
   orb: ['red', 'green', 'grey', 'grey', 'grey', 'green', 'grey', 'red', 'grey']
 }
 
 module.exports = function(controller) {
-  
+
   controller.on("code_entered", function(params) {
-    
+
     var bot = params.bot;
     controller.storage.teams.get(params.team).then(res => {
-      
+
       var correctCodes;
       var codeObj;
-            
+
       if (['safe', 'bookshelf', 'aris_door', 'remote'].includes(params.codeType)) {
         if (params.codeType == 'safe') {
           correctCodes = controller.safeCode;
@@ -36,63 +36,63 @@ module.exports = function(controller) {
           _.each(controller.remoteCombos[parseInt(params.channel) - 1], function(item, ind) {
             correctCodes[ind] = controller.remoteCodes[item];
           });
-          
+
         }
-        
+
         codeObj = checkCodeObject(params.code, correctCodes);
         codeObj.code = params.codeType;
-        
+
       } else {
-        
+
         if (params.codeType == "buttons") {
           correctCodes = correctButtonCodes;
-        } 
+        }
         else if (params.codeType == 'telegraph_key') {
           correctCodes = controller.telegraphKeys;
           params.code = [params.code[0], params.code[1], params.code[2]];
-        } 
+        }
         else if (params.codeType == 'keypad') {
           correctCodes = controller.keypadCode;
         }
-        
+
         codeObj = checkCodeArray(params.code, correctCodes);
 
       }
-        
+
       if (params.codeType == "telegraph_key")
         codeObj.puzzle = parseInt(codeObj.puzzle) + 1;
-      else if (params.codeType == 'remote') 
+      else if (params.codeType == 'remote')
         codeObj.puzzle = parseInt(params.channel);
-      
+
       // Store data of entered code
-      controller.dataStore(params.event, "code", { 
-        codeObj: codeObj, 
+      controller.dataStore(bot, params.event, "code", { 
+        codeObj: codeObj,
         codeType: params.codeType
       }).then(data => console.log("logged: ", data))
         .catch(err => console.log("code loggin error: ", err));
-            
+
       if (codeObj.correct == true) {
-        
+
         params.key = codeObj;
         params.team = res;
-        
+
         controller.trigger("state_change", [params]);
 
         if (codeObj.puzzle == 'safari')
           controller.trigger("image_counter_onboard", [bot, params.event]);
 
       } else {
-                    
+
          var vars = {
            sameScript: false
          };
-        
+
         if (params.codeType == 'bookshelf') {
-          
+
           controller.randomText(function(text) {
-            
+
             vars.randomText = text;
-            
+
             controller.makeCard(bot, params.event, params.codeType, 'random', vars, function(card) {
                 console.log(card, "is the card for the wrong entered code");
 
@@ -100,10 +100,10 @@ module.exports = function(controller) {
                 bot.replyInteractive(params.event, card);
 
              });
-            
+
           });
         } else {
-          
+
          controller.makeCard(bot, params.event, params.codeType, 'wrong', vars, function(card) {
             console.log(card, "is the card for the wrong entered code");
 
@@ -113,34 +113,34 @@ module.exports = function(controller) {
          });
         }
       }
-            
-      
+
+
     });
-  
-    
+
+
   }); // End on event
-  
+
   // Function checks if the entered code matches any of an Array of correct codes
   // Will return 'correct' based on match (true for match)
   // Will return 'puzzle' as the correct code name if there is a match
   // Will return 'code' as the entered code
   var checkCodeArray = function(code, correctCodes) {
     var overall = {};
-    
+
     // Loop through Array
     for (var key in correctCodes) {
-      
+
       overall[key] = [];
-      
+
       _.each(correctCodes[key], function(c, i) {
         overall[key].push(c == code[i]);
       });
-      
+
       // Compare correct and entered code
       // If match, return
       if (JSON.stringify(correctCodes[key]) == JSON.stringify(code)) {
           return { correct: true, puzzle: key, code: code };
-      } 
+      }
 
     }
 
@@ -148,30 +148,30 @@ module.exports = function(controller) {
     return { correct: false, code: code, overall: overall };
 
   }
-  
+
   // Checks entered code against a single correct code Object
   // Will return 'correct' based on match (true for match)
   // Will return 'code' as the entered code
   // Will return 'overall' as an object of true/false matches for each entry
   var checkCodeObject = function(code, correctCode) {
-    
+
     var count = 0;
     var overall = {};
     code = sort(code);
     correctCode = sort(correctCode);
-    
+
     // Loop through object keys
     for (var key in correctCode) {
-      
+
       // Set the overall key to be true or false based on match
       // Will make up an object of true/false results for each button or menu
       overall[key] = correctCode[key] == code[key];
-      
+
       // If this is a match, count up
       if (overall[key]) {
 
         count ++;
-        // If all keys match, return 
+        // If all keys match, return
         if ( count == Object.keys(correctCode).length ) {
            return { correct: true, code: code, overall: overall };
         }
@@ -179,9 +179,9 @@ module.exports = function(controller) {
       }
 
     }
-    // If not all keys match, return 
+    // If not all keys match, return
     return { correct: false, code: code, overall: overall };
-    
+
   }
-  
+
 }
