a4c622c9d711002506fb0e592f2942ab7ee00f50
erica-salling <erica.salling@generalassemb.ly>
2018-09-15 15:02:09 -0400

    Consoles

--- skills/data_store.js
+++ skills/data_store.js
@@ -146,7 +146,7 @@ module.exports = function(controller) {
 
         controller.findRelatedMsg(bot, message, team.bot.app_token).then(msg => {
 
-          // console.log(msg, " is what the promise returned related message");
+          console.log(msg, " is what the promise returned related message");
 
           if (msg) {
             dataEvent.relatedMsg = msg.id;
@@ -154,7 +154,7 @@ module.exports = function(controller) {
 
           controller.eventStages(bot, event, type).then(res => {
 
-            // console.log(res, " is what the promise returned");
+            console.log(res, " is what the promise returned");
 
             dataEvent.place = res;
 
